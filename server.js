9db5bc33430e77afd7d43625852aac37bfd6e32e
Toby Fee <tobyfee@gmail.com>
2017-09-26 17:06:49 -0700

    added route for loading all recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +18,17 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
+// we're going to add some items to Recipes
+// so there's some data to look at
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('milk chocolate', ['cocoa butter', 'milk', 'sugar']);
+
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
